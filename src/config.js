fea7a55d41057d641249604e168bc95e995dc927
Rosa Flores Lopez <rosa.lopez@edu.techstarter.de>
2024-10-28 16:00:44 +0100

    update

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // config.js
 export const API_URL = 'http://localhost:3030/api/products';
-export const BACKEND_URL = '/products'; // Añade esto si es necesario
+export const BACKEND_URL = 'http://localhost:3030/api/products'; // Añade esto si es necesario
